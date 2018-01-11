80f29a42ef81b14c2dd22869e5d00f0f58d98126
Justin de Leon <jusmdeleon@gmail.com>
2018-01-11 15:40:16 +0800

    Add vehicles table migration

---
+++ database/migrations/2018_01_11_072403_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('engine_displacement_value');
+            $table->enum('engine_displacement_uom', ['L', 'CC', 'CID']);
+            $table->float('engine_power');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
