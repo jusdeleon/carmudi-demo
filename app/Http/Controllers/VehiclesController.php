9a3adc4a6ce172fe07b86ac6885130981e8919fc
Justin de Leon <jusmdeleon@gmail.com>
2018-01-11 16:07:39 +0800

    Add GET /vehicles endpoint

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Vehicle;
+
 class VehiclesController extends Controller
 {
+    public function index()
+    {
+        return Vehicle::all();
+    }
 }
