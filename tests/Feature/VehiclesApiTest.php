fff897be04fb9bc18353202468d8a068a3bf56de
Justin de Leon <jusmdeleon@gmail.com>
2018-01-12 11:32:34 +0800

    Add validation tests

--- tests/Feature/VehiclesApiTest.php
+++ tests/Feature/VehiclesApiTest.php
@@ -25,11 +25,53 @@ class VehiclesApiTest extends TestCase
                     'engine_displacement_value',
                     'engine_displacement_uom',
                     'engine_power',
-                ]
-            ]
+                ],
+            ],
         ]);
     }
-    
+
+    /** @test */
+    public function fields_are_required_when_creating_a_vehicle()
+    {
+        $response = $this->post('vehicles', []);
+
+        $response->assertResponseStatus(422);
+
+        $response->seeJson([
+            'name' => [
+                'The name field is required.',
+            ],
+            'engine_displacement_value' => [
+                'The engine displacement value field is required.',
+            ],
+            'engine_displacement_uom' => [
+                'The engine displacement uom field is required.',
+            ],
+            'engine_power' => [
+                'The engine power field is required.',
+            ],
+        ]);
+    }
+
+    /** @test */
+    public function the_engine_displacement_uom_field_does_not_accept_invalid_values()
+    {
+        $response = $this->post('vehicles', [
+            'name' => 'Car',
+            'engine_displacement_value' => 1.5,
+            'engine_displacement_uom' => 'ML',
+            'engine_power' => 3.5,
+        ]);
+
+        $response->assertResponseStatus(422);
+
+        $response->seeJson([
+            'engine_displacement_uom' => [
+                'The selected engine displacement uom is invalid.',
+            ],
+        ]);
+    }
+
     /** @test */
     public function it_can_create_a_vehicle()
     {
@@ -58,7 +100,7 @@ class VehiclesApiTest extends TestCase
                 'engine_displacement_value' => 1.5,
                 'engine_displacement_uom' => 'L',
                 'engine_power' => 3.5,
-            ]
+            ],
         ]);
     }
 }
